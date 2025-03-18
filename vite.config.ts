82d7ef808f8193870a8ae6e7ddf5b3c16a17f164
N3zZa <necrofos9@gmail.com>
2025-03-18 09:27:48 +0300

    config: vercel.json and vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import eslint from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react(), tailwindcss(), eslint()],
+   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   optimizeDeps: {
     include: ['recharts'],
   },
