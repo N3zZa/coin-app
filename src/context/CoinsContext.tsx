56814f60303b978050c90f678d46535c116db5c0
N3zZa <necrofos9@gmail.com>
2025-03-20 22:35:25 +0300

    refactor: added comments

--- src/context/CoinsContext.tsx
+++ src/context/CoinsContext.tsx
@@ -5,18 +5,18 @@ import { PortfolioCoinsId } from 'types/PortfolioCoinsIdModel';
 import { LocalStorageService } from 'utils/localStorage';
 
 export interface CoinsContextType {
-  assets: AssetItemModel[];
-  loading: boolean;
-  error: string | null;
-  portfolioCoinsId: PortfolioCoinsId[];
-  addPortfolioItem: (asset: AssetItemModel, amount: number) => void;
-  portfolioPrice: number;
-  portfolioCoins: AssetItemModel[];
+  assets: AssetItemModel[]; // api main assets
+  loading: boolean; // loader
+  error: string | null; // error
+  portfolioCoinsId: PortfolioCoinsId[]; // portfolio coins from localstorage
+  addPortfolioItem: (asset: AssetItemModel, amount: number) => void; // fun on adding a coin to the portfolio
+  portfolioPrice: number; // price from api
+  portfolioCoins: AssetItemModel[]; // portfolio coins that we add by add button
   setPortfolioCoins: React.Dispatch<React.SetStateAction<AssetItemModel[]>>;
-  initialPortfolioPrice: number;
-  clearPortfolio: () => void;
-  removePortfolioCoin: (coinID: string) => void;
-  refeshCoins: () => void;
+  initialPortfolioPrice: number; // initial price that we add to the portfolio
+  clearPortfolio: () => void; // clear portfolio func
+  removePortfolioCoin: (coinID: string) => void; // remove portfolio coin func
+  refeshCoins: () => void; // getting coins from the api func
 }
 
 export const CoinsContext = createContext<CoinsContextType | undefined>(undefined);
@@ -25,37 +25,47 @@ const PORTFOLIOITEMS_KEY = 'portfolioItems';
 const INITIAL_PORTFOLIO_PRICE_KEY = 'initialPortfolioPrice';
 
 export const CoinsProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  // state of main table assets
   const [assets, setAssets] = useState<AssetItemModel[]>([]);
+  // loader and error states
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
+  // state of portfolioCoinsId ({id, amoint}), needed for localstorage
   const [portfolioCoinsId, setPortfolioCoinsId] = useState<PortfolioCoinsId[]>(
     () => LocalStorageService.getItem<PortfolioCoinsId[]>(PORTFOLIOITEMS_KEY) || [],
   );
 
+  // state of the price received from the api
   const [portfolioPrice, setPortfolioPrice] = useState<number>(0);
 
+  // state of the initial price that we add to the portfolio
   const [initialPortfolioPrice, setInitialPortfolioPrice] = useState<number>(
     () => LocalStorageService.getItem<number>(INITIAL_PORTFOLIO_PRICE_KEY) || 0,
   );
 
+  // state of portfolio coins that we add by add button
   const [portfolioCoins, setPortfolioCoins] = useState<AssetItemModel[]>([]);
 
+  // useEffect controls localstorage state changes
   useEffect(() => {
     LocalStorageService.setItem(PORTFOLIOITEMS_KEY, portfolioCoinsId);
     LocalStorageService.setItem(INITIAL_PORTFOLIO_PRICE_KEY, initialPortfolioPrice);
   }, [portfolioCoinsId, initialPortfolioPrice]);
 
+  // func to getting coins from the api
   const refeshCoins = () => {
     fetchCoins({ setAssets, setLoading, setError });
   };
 
+  // getting coins from the api when the page loads
   useEffect(() => {
     setLoading(false);
     setError(null);
     refeshCoins();
   }, []);
 
+  // calculatung the api price when assets(api items) or portfolioCoinsId changes
   useEffect(() => {
     const filteredItemsPrice = assets
       .filter((item) => portfolioCoinsId.some((portfolioItem) => portfolioItem.id === item.id))
@@ -69,6 +79,7 @@ export const CoinsProvider: React.FC<{ children: ReactNode }> = ({ children }) =
     }
   }, [assets, portfolioCoinsId]);
 
+  // calculating the initial price when portfolioCoins(cur added items) or initialPortfolioPrice changes
   useEffect(() => {
     const getPortfolioPrice = () => {
       return portfolioCoins.reduce((acc, item) => {
@@ -85,6 +96,7 @@ export const CoinsProvider: React.FC<{ children: ReactNode }> = ({ children }) =
     }
   }, [portfolioCoins, initialPortfolioPrice]);
 
+  // func of adding items
   const addPortfolioItem = (asset: AssetItemModel, amount: number = 1) => {
     setInitialPortfolioPrice((prev) => prev + asset.priceUsd);
     setPortfolioCoins((prev) =>
@@ -109,16 +121,14 @@ export const CoinsProvider: React.FC<{ children: ReactNode }> = ({ children }) =
     });
   };
 
-  /*  const isInPortfolio = (coinID: string) => {
-    return portfolioCoinsId.some((item) => item.id === coinID);
-  }; */
-
+  // func of removing an item from  the portfolio
   const removePortfolioCoin = (coinID: string) => {
     setPortfolioCoins((prev) => prev.filter((item) => item.id !== coinID));
     setPortfolioCoinsId((prev) => prev.filter((item) => item.id !== coinID));
     setInitialPortfolioPrice(0);
   };
 
+  // func of clearing items from the portfolio
   const clearPortfolio = () => {
     setPortfolioCoins([]);
     setPortfolioCoinsId([]);
